c1554e66a32af9db1d256be5c2f4468863bc6ef5
ARMEDIA\bojan.mickoski <bojan.mickoski@armedia.com>
2022-11-30 19:05:03 +0100

    convert infix to postfix array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   expression = '5 + 10 * (5 + 2 + 4)';
-  expressionWithDigits = '5.5 + 10 * (5 + 2 + 4)';
+  complexExpression = '3 * (4 - 2) + 1 * 5';
 
   constructor(private evaluatorService: EvaluatorService) {}
   ngOnInit(): void {
-    console.log(
-      this.evaluatorService.extractNumbersAndOperands(this.expression)
-    );
-    console.log(
-      this.evaluatorService.extractNumbersAndOperands(this.expressionWithDigits)
-    );
+    console.log(this.evaluatorService.evaluate(this.expression));
+    console.log(this.evaluatorService.evaluate(this.complexExpression));
   }
 }
--- src/app/services/evaluator.service.ts
+++ src/app/services/evaluator.service.ts
@@ -1,45 +1,138 @@
 import { Injectable } from '@angular/core';
 
+const precedence: any = { '*': 1, '/': 1, '+': 0, '-': 0 };
+const leftParenthesis = '(';
+const rightParenthesis = ')';
+
 @Injectable({ providedIn: 'root' })
 export class EvaluatorService {
-  public extractNumbersAndOperands(input: string): (string | number)[] {
+  /**
+   * Extract numbers and operands in array
+   *
+   * @param input Input
+   * @returns array with numbers and operands
+   */
+  private extractNumbersAndOperands(input: string): (string | number)[] {
     let iterator = 0;
-    const tokens = [];
+    const chars = [];
 
     while (iterator < input.length) {
       const char = input[iterator];
 
-      // If the character is a number add it tokens
+      // If the character is a number...
       if (/[0-9]/.test(char)) {
+        // Create a string to hold all the digits in the number
         let digits = '';
 
+        // While we have more digits (or a period) in the number and we haven't
+        // gotten to the end of the input...
         while (iterator < input.length && /[0-9\.]/.test(input[iterator])) {
           digits += input[iterator++];
         }
 
+        // Convert the digits to a number and push the number to the array of chars
         const number = parseFloat(digits);
-        tokens.push(number);
-        iterator++;
+        chars.push(number);
         continue;
       }
 
-      // If the character is a operand push it to tokens
+      // If the character is a operand push it to chars
       if (/[+\-/*(),^<>=]/.test(char)) {
-        tokens.push(char);
+        chars.push(char);
         iterator++;
         continue;
       }
 
-      // If the character is white space ignore it
+      // If the character is white space...
       if (char === ' ') {
+        // Ignore it
         iterator++;
         continue;
       }
 
       // If the character can't recognize, throw an error
-      throw new Error(`Invalid token ${char} at position ${iterator}`);
+      throw new Error(`Invalid char ${char} at position ${iterator}`);
     }
 
-    return tokens;
+    return chars;
+  }
+
+  /**
+   * Convert infix to postfix polish notation
+   *
+   * @param chars chars
+   * @returns transformed postfix array of chars
+   */
+  private convertInfixToPostfix(chars: (string | number)[]) {
+    const operators: string[] = [];
+    const postfixArray: any[] = [];
+
+    for (let i = 0; i < chars.length; i++) {
+      const char = chars[i];
+
+      // if char is a number push it to postfix array
+      if (typeof char === 'number') {
+        postfixArray.push(char);
+        continue;
+      }
+
+      // if char is an operator check if there are oeprators in operators stack
+      // with higher precedence than current one, if yes remove from operators and
+      // add it to postfix array and then push current token to operators
+      if (/[+\-/*<>=^]/.test(char)) {
+        while (this.checkEqualOrHigherPrecedence(operators, char)) {
+          postfixArray.push(operators.pop());
+        }
+        operators.push(char);
+        continue;
+      }
+
+      if (char === leftParenthesis) {
+        operators.push(char);
+        continue;
+      }
+
+      if (char === rightParenthesis) {
+        while (
+          operators.length > 0 &&
+          operators[operators.length - 1] !== leftParenthesis
+        ) {
+          postfixArray.push(operators.pop());
+        }
+        operators.pop();
+        continue;
+      }
+
+      throw new Error(`Unparsed char ${char} at position ${i}`);
+    }
+
+    for (let i = operators.length - 1; i >= 0; i--) {
+      postfixArray.push(operators[i]);
+    }
+
+    return postfixArray;
+  }
+
+  /**
+   * Check whether top operator in operators has a higher or equal prescedence of nextChar
+   *
+   * @param operators Operators
+   * @param nextChar next char
+   * @returns should be removed or not
+   */
+  private checkEqualOrHigherPrecedence(
+    operators: string[],
+    nextChar: string
+  ): boolean {
+    if (operators.length === 0) {
+      return false;
+    }
+
+    const lastOperator = operators[operators.length - 1];
+    return precedence[lastOperator] >= precedence[nextChar];
+  }
+
+  evaluate(input: string): any {
+    return this.convertInfixToPostfix(this.extractNumbersAndOperands(input));
   }
 }
